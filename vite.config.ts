5c5f83094490e34e4c0ff286ec75460b7eff8206
JoshuaKist <Joshua.Kistler@swisscom.com>
2025-11-22 10:22:40 +0100

    Hosting 5.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  base: "https://joshuakistler.github.io/TrainChecker/";
-  const env = loadEnv(mode, ".", "");
-  return {
-    server: {
-      port: 3000,
-      host: "0.0.0.0",
+export default defineConfig({
+  // Example: Accessing an environment variable
+  define: {
+    "process.env": process.env, // Ensure process.env is defined
+    "process.env.VITE_GEMINI_API_KEY": JSON.stringify(
+      process.env.VITE_GEMINI_API_KEY
+    ),
+  },
+  base: "/TrainChecker/",
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "."),
     },
-    plugins: [react()],
-    define: {
-      "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-    },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "."),
-      },
-    },
-    base: "/TrainChecker/",
-  };
+  },
 });
